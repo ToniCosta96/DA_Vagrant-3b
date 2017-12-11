b35a2adf74549f8716ad352039b5983be1d0f1fe
ToniCosta <ToniCosta96@gmail.com>
2017-12-11 10:03:12 +0100

    Actividad 10

--- provision.sh
+++ provision.sh
@@ -1,2 +1,7 @@
-sudo apt-get -y update
-sudo apt-get install -y apache2
+apt-get -y update
+apt-get install -y apache2
+apt-get install -y git
+mkdir /vagrant/html
+rm -r /var/www/html/
+ln -s /vagrant/html/ /var/www/html
+git clone https://github.com/jogich/reca.git /vagrant/html
